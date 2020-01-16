a0a74693a46c0bf7569f332ee39fda997250aacc
Ernesto Garcia Vazquez <egv@live.unc.edu>
2020-01-16 05:11:49 +0000

    This file was updated

--- jrep/build.sh
+++ jrep/build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Author: Ernesto Garcia Vazquez
-# PID: 730288977
-# I pledge the UNC and COMP290 Honor Code.
-
 # Compiles Jrep.java ﬁle into class ﬁles in bin/
 javac -d "bin/" "src/comp290/Jrep.java"
 
